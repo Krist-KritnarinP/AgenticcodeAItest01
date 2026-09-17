3def81113340ebe1487bc589ec5dc919bf6621cd
Krist-KritnarinP <krist.fws@gmail.com>
2026-09-17 17:48:08 +0700

    configure Better Auth environment variables

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,8 @@ import { nextCookies } from "better-auth/next-js";
 import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, { provider: "postgresql" }),
   emailAndPassword: { enabled: true },
   plugins: [nextCookies()],
